b11cf9f435768cf866f31c354fb538d42c40a185
abhilashpraharaj122@gmail.com <abhilashpraharaj122@gmail.com>
2024-07-19 02:54:16 +0530

    literals made

--- src/scanner.rs
+++ src/scanner.rs
@@ -15,6 +15,83 @@ impl Scanner {
 }
 
 #[derive(Debug)]
+pub enum TokenType {
+  // Single character tokens
+  LeftParen,
+  RightParen,
+  LeftBrace,
+  RightBrace,
+  Comma,
+  Dot,
+  Plus,
+  Minus,
+  Star,
+  Slash,
+  Start,
+  Semicolon,
+
+  // Multiple character tokens
+  Bang,
+  BangEqual,
+  Equal,
+  EqualEqual,
+  Greater,
+  GreaterEqual,
+  Less,
+  LessEqual,
+
+  // Literals
+  Identifier,
+  String,
+  Number,
+
+  // Keywords
+  And,
+  Class,
+  Warna,   // Else
+  Agar,    // If
+  Nope,    // Nil
+  Or,
+  For,
+  Chap,    // Print
+  Wapas,   // Return
+  Supper,
+  Sach,    // True
+  Jhoot,   // False
+  This,
+  Var,
+  Jabtak,  // While
+  Eof,
+}
+
+#[derive(Debug)]
+pub enum LiteralVal {
+  IntValue(i64),
+  FValue(f64),
+  StringValue(String),
+  IdentifierValue(String),
+}
+
+#[derive(Debug)]
 pub struct Token {
-  // Define fields for the Token struct
+  token_type: TokenType,
+  lexeme: String,
+  literal: Option<LiteralVal>,
+  line_number: i64,
+}
+
+impl Token {
+  pub fn new(
+      token_type: TokenType,
+      lexeme: String,
+      literal: Option<LiteralVal>,
+      line_number: i64,
+  ) -> Self {
+      Self {
+          token_type,
+          lexeme,
+          literal,
+          line_number,
+      }
+  }
 }
